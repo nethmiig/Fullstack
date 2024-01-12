2b9cd1d1bb9b9b29d216d782b20206efb03702b4
Nethmi Gamage <enega002@edu.xamk.fi>
2024-01-12 22:18:47 +0200

    completed exercise

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let paragraph = document.querySelector("#description");
+let btn = document.querySelector("#actionButton");
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
+btn.addEventListener("click", function(){
+    paragraph.style.height = getComputedStyle(paragraph).height;
+    paragraph.innerHTML = '';
+});
+
+
+
+
+/* 
 
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 // JavaScript to dynamically set the current year
@@ -12,7 +24,7 @@ function toggleParagraph() {
 
     // Check the current display property
     if (style.display === 'none' || style.display === '') {
-        // If hidden, make it visible
+        // If hidden, make it visibled
         paragraph.style.display = 'block';
         actionButton.innerHTML = 'Click Here!';
         
@@ -21,11 +33,10 @@ function toggleParagraph() {
         // If visible, make it hidden
         paragraph.style.display = 'none';
         actionButton.innerHTML = 'You Clicked!';
+        actionButton.style.marginBottom = '0';
 
     }
    
 }
 
-
-
-
+ */
